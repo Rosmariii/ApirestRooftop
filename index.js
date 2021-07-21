3df6599bf0398c49516f18dbaaacc72f65a6e072
Rosmari <rosmarifernandez@hotmail.com>
2021-07-20 22:56:36 -0300

    middleware log

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const dotenv = require("dotenv")
-var audit = require('express-requests-logger')
+const fs = require('fs')
 
 dotenv.config()
 
@@ -11,25 +11,46 @@ const authenticateJWT = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if (authHeader) {
+        console.log('a')
         const token = authHeader.split(' ')[1];
 
         if (token == accessTokenSecret) {
-            return res.sendStatus(200).json({message:"authorized"})
+           
+             res.status(200).json({message:"authorized"})       
         } else {
-
-            return res.sendStatus(403).json({message:"unauthorized"})
+           
+             res.status(403).json({message:"unauthorized"})      
         }
-    } else {
-        res.sendStatus(404).json({message:"error"});
-    } next()
+    } else {   
+         return res.status(404).json({message:"error"});    
+    }
+   
+    next()
 };
+
+const log = (req, res, next) => {
+    
+    let date = new Date()
+    let titulo = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+    let horas = date.getHours().toString().padStart(2, '0')
+    let minutos = date.getMinutes().toString().padStart(2, '0')
+    let segundos = date.getSeconds().toString().padStart(2, '0')
+
+    let hora_evento = `${date.getFullYear()}`
+    let {ip, url, method} = req
+    
+    fs.appendFile(`${titulo}.log`, `${ip}, ${titulo} ${horas}:${minutos}:${segundos}, ${method}, ${url}\n`, (error) => {
+        if (error) {
+            throw error; 
+        }
+    })
+    next()
+}
+
+app.use(log)
 
 app.get('/', authenticateJWT, (req, res) => {
     res.send('holamundo');
 });
-
-app.use(audit({
-    logger: logger,
-
 
 app.listen(process.env.PORT)
